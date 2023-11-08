6a90555e50debfd0fe71f2f192af45ab3416c1b3
BroBeans <BroBeansCPG@users.noreply.github.com>
2023-11-08 17:02:45 +0800

    inconsistent spacing

--- CTB_Splash/spe/config.cpp
+++ CTB_Splash/spe/config.cpp
@@ -14,9 +14,9 @@ class CfgPatches
 
 class CfgMainMenuSpotlight
 {
-  delete SPE_Arsenal;
-  delete SPE_Campagin_OperationCobra_04;
-  delete SPE_Coop_DerZahnarzt;
-  delete SPE_Faction_Showcase_GER;
-  delete SPE_Faction_Showcase_US;
+	delete SPE_Arsenal;
+	delete SPE_Campagin_OperationCobra_04;
+	delete SPE_Coop_DerZahnarzt;
+	delete SPE_Faction_Showcase_GER;
+	delete SPE_Faction_Showcase_US;
 };
